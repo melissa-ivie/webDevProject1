3f69b27ecd19a137186265a5f2a8d7b5986fed9d
Bryson Steck <steck.bryson@gmail.com>
2022-02-22 17:26:27 -0700

    fixed addingprojects class name

--- server/database/migrations/1645118952470-AddingProjects.ts
+++ server/database/migrations/1645118952470-AddingProjects.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class AddingProject1645109419318 implements MigrationInterface {
+export class AddingProject1645118952470 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
